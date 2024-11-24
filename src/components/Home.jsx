5972a244a3057cd33a25119ca26358ef0fc39fb2
Suegathh <suzzainnegathu@gmail.com>
2024-11-24 13:46:23 +0300

    edited home.css

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,7 @@
-
+import './Home.css';
 function Home() {
     return (
-        <div className='Home'>
+        <div className='home'>
           <h1>My Blog</h1>
           <h2>Welcome to my React Blog</h2>
           <p>This is my first blog built while learning React</p>
