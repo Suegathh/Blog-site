dea4244313d2b2c4da9ca572fb2d6f5c231f7356
Suegathh <suzzainnegathu@gmail.com>
2024-11-24 11:57:16 +0300

    Blog.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
 import About from './components/About';
-import Blog from './components/blog';
+import Blog from './components/Blog';
 import BlogDetails from './components/BlogDetails';
 import Navbar from './components/Navbar';
 import Photo from './components/ProfilePhoto';
